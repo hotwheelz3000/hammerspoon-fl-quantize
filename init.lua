a26731171834d14c33813825a53dcc3037a3fbe4
hotwheelz3000 <149254166+hotwheelz3000@users.noreply.github.com>
2026-06-09 00:28:49 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,32 +1,35 @@
+-- FL Studio Quick Quantize
+-- Tap Cmd+Q to quantize, hold 1.5s to quit
+-- =====================
 local killTimer = nil
 local flApp = nil
 
-hs.hotkey.bind({"cmd"}, "Q", 
-  function()
-    -- key down
-    flApp = hs.application.frontmostApplication()
-    if flApp:name() == "FL Studio" then
-      flApp:activate()
-      hs.timer.doAfter(0.3, function()
-        hs.eventtap.keyStroke({"cmd", "alt"}, "Q")
-      end)
-      killTimer = hs.timer.doAfter(0.5, function()
-        if flApp then
-          flApp:kill()
-        end
-      end)
-    else
-      flApp:kill()
-      flApp = nil
-    end
-  end, 
-  function()
-    -- key up
-    if killTimer then
-      killTimer:stop()
-      killTimer = nil
-    end
-    flApp = nil
-  end,
-  nil
+hs.hotkey.bind({"cmd"}, "Q",
+ function()
+   flApp = hs.application.frontmostApplication()
+   if flApp and flApp:name() == "FL Studio" then
+     flApp:activate()
+     hs.timer.doAfter(0.3, function()
+       hs.eventtap.keyStroke({"cmd", "alt"}, "Q")
+     end)
+     killTimer = hs.timer.doAfter(1.5, function()
+       if flApp then
+         flApp:kill()
+         flApp = nil
+         killTimer = nil
+       end
+     end)
+   elseif flApp then
+     flApp:kill()
+     flApp = nil
+   end
+ end,
+ function()
+   if killTimer then
+     killTimer:stop()
+     killTimer = nil
+   end
+   flApp = nil
+ end,
+ nil
 )
